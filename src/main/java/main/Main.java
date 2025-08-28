0b65ef593117f8989696d5907bd5675f919a1bdb
viniguerra <viniciusguerra961@gmail.com>
2025-08-28 12:20:15 -0300

    bug correction

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,40 @@
 package main;
 
-public class Main {
+import net.viniguerra.core.Window;
+import org.lwjgl.glfw.GLFW;
+
+public class Main implements Runnable {
+    public Thread game;
+    public static Window window;
+    public static final int WIDTH = 1280, HEIGHT = 760;
+
+    public void start() {
+        game = new Thread(this, "game");
+        game.start();
+    }
+
+    public static void init() {
+        window = new Window(WIDTH, HEIGHT, "Game");
+        window.create();
+    }
+
+    public void run() {
+        init();
+        while (!window.shouldClose()) {
+            update();
+            render();
+        }
+    }
+
+    private void update() {
+        window.update();
+    }
+
+    private void render() {
+        window.swapBuffers();
+    }
+
+    public static void main(String[] args) {
+        new Main().start();
+    }
 }
